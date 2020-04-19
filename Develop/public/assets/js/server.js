b9f36185eb5ae24fc41873976943f37eba099cf5
The Mechanic <jaber4132@gmail.com>
2020-04-19 15:08:33 -0400

    Step 3 - Added connectino to html files in server.js

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -7,6 +7,18 @@ const port = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../index.html"));
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../../notes.html"));
+})
+
+app.get("/assets/js/index.js", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.js"));
+})
+
 app.listen(port, function() {
     console.log(`App Server is listening on PORT: ${port}`);
 });
